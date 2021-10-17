bd7f0e83d3f1cfba2a8888db676e415906321971
Yony Calsin <helloyonycalsin@gmail.com>
2021-10-16 23:31:20 -0500

    feat: 🎸 add url script internal

---
+++ scripts/internal/url.ts
@@ -0,0 +1,29 @@
+import { fileURLToPath, pathToFileURL } from 'url'
+
+export default class URL extends globalThis.URL {
+  to(...segments: string[]) {
+    return segments.reduce((url: URL, segment) => new URL(segment, url), this)
+  }
+
+  includes(searchString: string, position = 0) {
+    return this.href.includes(searchString, position)
+  }
+
+  endsWith(searchString: string, length = this.href.length) {
+    return this.href.endsWith(searchString, length)
+  }
+
+  get dir() {
+    return new URL(this.href + '/')
+  }
+
+  get ospathname() {
+    return fileURLToPath(this)
+  }
+
+  static from(segment: string | globalThis.URL, ...segments: string[]) {
+    return (/^file:/.test(segment as string) ? new URL(segment) : new URL(pathToFileURL(segment as string))).to(
+      ...segments,
+    )
+  }
+}
