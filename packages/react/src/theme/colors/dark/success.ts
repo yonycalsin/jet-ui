40aa74a2a29307fe7a1d9acd6a3f1718acd2693d
Yony Calsin <helloyonycalsin@gmail.com>
2021-10-17 16:25:54 -0500

    feat: 🎸 add success color

---
+++ packages/react/src/theme/colors/dark/success.ts
@@ -0,0 +1,14 @@
+export const success = {
+  success1: 'hsl(146 30.0% 7.4%)',
+  success2: 'hsl(155 44.2% 8.4%)',
+  success3: 'hsl(155 46.7% 10.9%)',
+  success4: 'hsl(154 48.4% 12.9%)',
+  success5: 'hsl(154 49.7% 14.9%)',
+  success6: 'hsl(154 50.9% 17.6%)',
+  success7: 'hsl(153 51.8% 21.8%)',
+  success8: 'hsl(151 51.7% 28.4%)',
+  success9: 'hsl(151 55.0% 41.5%)',
+  success10: 'hsl(151 49.3% 46.5%)',
+  success11: 'hsl(151 50.0% 53.2%)',
+  success12: 'hsl(137 72.0% 94.0%)',
+}
---
+++ packages/react/src/theme/colors/light/success.ts
@@ -0,0 +1,14 @@
+export const success = {
+  success1: 'hsl(136 50.0% 98.9%)',
+  success2: 'hsl(138 62.5% 96.9%)',
+  success3: 'hsl(139 55.2% 94.5%)',
+  success4: 'hsl(140 48.7% 91.0%)',
+  success5: 'hsl(141 43.7% 86.0%)',
+  success6: 'hsl(143 40.3% 79.0%)',
+  success7: 'hsl(146 38.5% 69.0%)',
+  success8: 'hsl(151 40.2% 54.1%)',
+  success9: 'hsl(151 55.0% 41.5%)',
+  success10: 'hsl(152 57.5% 37.6%)',
+  success11: 'hsl(153 67.0% 28.5%)',
+  success12: 'hsl(155 40.0% 14.0%)',
+}
